28e23a8456be9994c07cdb62648025aeaeb27ba0
Muhammadazim <shuhrataliyevm@gmail.com>
2025-02-26 22:27:31 +0500

    new.preckt.asaxiy.uz

--- src/components/InputPhone/InputPhone.jsx
+++ src/components/InputPhone/InputPhone.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useState, useMemo, memo, useCallback } from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 
-function MyPhoneInput() {
+// eslint-disable-next-line react/display-name
+const MyPhoneInput = memo(() => {
     const [phone, setPhone] = useState('');
+
+    const phoneMask = useMemo(() => ({ uz: '(..) ...-..-..' }), []);
+    const handleChange = useCallback((value) => {
+        setPhone(value);
+    }, []);
 
     return (
         <div className='input-phone'>
@@ -11,11 +17,11 @@ function MyPhoneInput() {
             <PhoneInput
                 country={'uz'}
                 value={phone}
-                onChange={setPhone}
-                masks={{ uz: '(..) ...-..-..' }}
+                onChange={handleChange}
+                masks={phoneMask}
             />
         </div>
     );
-}
+});
 
 export default MyPhoneInput;
